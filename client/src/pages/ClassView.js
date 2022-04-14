e715f8db8235238cef10b044be1e6c52a8ba3006
Jonathan Mesa <mesajonathan@ufl.edu>
2022-04-14 13:01:40 -0400

    before valentian pushes

--- client/src/pages/ClassView.js
+++ client/src/pages/ClassView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import './ClassView.css'
+import './ClassView.css' 
+//import { syllabusScrape } from '../webscraper';
 
 function ClassView() {
 
@@ -9,37 +10,13 @@ function ClassView() {
     const course = location.state.course
 
     //for getting the schedule using web scraping
-    /*
-    const puppeteer = require("puppeteer"); // ^13.5.1
-    let browser;
-    (async () => {
-        const searchQuery = course.name + " " + course.code + " UF syllabus";
-
-        browser = await puppeteer.launch();
-        const [page] = await browser.pages();
-        await page.goto("https://www.google.com/", {waitUntil: "domcontentloaded"});
-        await page.waitForSelector('input[aria-label="Search"]', {visible: true});
-        await page.type('input[aria-label="Search"]', searchQuery);
-        await Promise.all([
-            page.waitForNavigation(),
-            page.keyboard.press("Enter"),
-        ]);
-        await page.waitForSelector(".LC20lb", {visible: true});
-        const searchResults = await page.$$eval(".LC20lb", els => 
-            els.map(e => ({title: e.innerText, link: e.parentNode.href}))
-        );
-        console.log(searchResults[0].link);
-    })()
-        .catch(err => console.error(err))
-        .finally(() => browser?.close())
-    ;
-    */
+    //const link = syllabusScrape(course)
 
 
     return(
         <div className='course'>
             <h1>{course.name} ({course.code})</h1>
-            <h2>Course Syllabus: </h2>
+            <h2>Course Syllabus:  {}</h2>
             <h2>{course.pres}</h2>
             <h2>Course ID: {course.id}</h2>
             <h2>Description:</h2>
---
+++ client/src/pages/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Homescreen() {
+
+    return(
+        <div className='style'>
+            <h1>hi</h1>
+        </div>
+    )
+}
+
+export default Homescreen
